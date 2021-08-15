72ea7745109385128fe05d25e38701fc2dda5710
Victor Kennysteve <64693405+vkennysteve@users.noreply.github.com>
2021-08-15 14:06:50 +0800

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -10,6 +10,11 @@ def primary():
 last = len(quotes) - 1
 rnd = random.randint(0, last)
   print(quotes[rnd])
+  
+  quotes.push("Great work!")
+  del(newline(quotes))
+  
+  print(quotes[20])
 
 if __name__== "__main__":
   primary()
