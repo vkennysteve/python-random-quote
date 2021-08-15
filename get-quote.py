47c339c92a6b7154ee3e9e2cbf4aa8ceefa6a922
Victor Kennysteve <64693405+vkennysteve@users.noreply.github.com>
2021-08-15 13:46:30 +0800

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,3 +1,5 @@
+import random
+
 def primary():
   print("Keep it logically awesome.")
 
@@ -5,7 +7,9 @@ def primary():
   quotes = f.readlines()
   f.close()
 
-  print(quotes)
+last = len(quotes) - 1
+rnd = random.randint(0, last)
+  print(quotes[rnd])
 
 if __name__== "__main__":
   primary()
